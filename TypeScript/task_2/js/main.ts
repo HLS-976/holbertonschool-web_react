df31efa8424a4ca99f33b869e38077f712e3abab
hls <hilliassmirali@gmail.com>
2025-05-28 12:51:25 +0200

    create function is_director

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -38,9 +38,22 @@ class Teacher implements TeacherInterface {
   }
 }
 
+
 function createEmployee(salary: number | string): Director | Teacher {
     if (typeof salary === 'number' && salary < 500) {
     return new Teacher();
   }
   return new Director();
+}
+
+function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  } else {
+    return employee.workTeacherTasks();
+  }
 }
