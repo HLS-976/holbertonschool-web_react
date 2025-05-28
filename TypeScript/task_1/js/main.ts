1faf1e52bc0165ec030e2ad691f5622fdadb70e5
hls <hilliassmirali@gmail.com>
2025-05-28 10:05:49 +0200

    task2

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -7,12 +7,17 @@ interface Teacher {
   [key: string]: any
 };
 
-const teacher3: Teacher = {
+interface Directors extends Teacher {
+  numberOfReports: number;
+};
+
+const teacher3: Directors = {
   firstName: 'John',
   fullTimeEmployee: false,
   lastName: 'Doe',
   location: 'London',
   contract: false,
+  numberOfReports: 17
 };
 
 console.log(teacher3);
